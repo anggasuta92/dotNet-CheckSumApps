c56c3e0ef2d68d1227ad86d2d40f3759a4acc56c
anggasuta92 <angga.suta92@gmail.com>
2020-03-08 19:53:47 +0800

    commit

---
+++ checkSumApps/frmCheckSum.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCheckSum
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtBineary = New System.Windows.Forms.TextBox()
+        Me.cmdGenerate = New System.Windows.Forms.Button()
+        Me.txtResult = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtBinearResult = New System.Windows.Forms.TextBox()
+        Me.panelLoading = New System.Windows.Forms.Panel()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.cmdReset = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.lblPersent = New System.Windows.Forms.Label()
+        Me.panelLoading.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtBineary
+        '
+        Me.txtBineary.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.txtBineary.Font = New System.Drawing.Font("Consolas", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBineary.ForeColor = System.Drawing.Color.White
+        Me.txtBineary.Location = New System.Drawing.Point(12, 25)
+        Me.txtBineary.Multiline = True
+        Me.txtBineary.Name = "txtBineary"
+        Me.txtBineary.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtBineary.Size = New System.Drawing.Size(288, 392)
+        Me.txtBineary.TabIndex = 0
+        '
+        'cmdGenerate
+        '
+        Me.cmdGenerate.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdGenerate.Location = New System.Drawing.Point(12, 422)
+        Me.cmdGenerate.Name = "cmdGenerate"
+        Me.cmdGenerate.Size = New System.Drawing.Size(168, 36)
+        Me.cmdGenerate.TabIndex = 1
+        Me.cmdGenerate.Text = "Generate Checksum"
+        Me.cmdGenerate.UseVisualStyleBackColor = True
+        '
+        'txtResult
+        '
+        Me.txtResult.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.txtResult.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.txtResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtResult.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtResult.ForeColor = System.Drawing.Color.White
+        Me.txtResult.Location = New System.Drawing.Point(310, 25)
+        Me.txtResult.Multiline = True
+        Me.txtResult.Name = "txtResult"
+        Me.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtResult.Size = New System.Drawing.Size(426, 433)
+        Me.txtResult.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(9, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(206, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Masukkan Kode Binear Disini..."
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(309, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(90, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Log Proses..."
+        '
+        'txtBinearResult
+        '
+        Me.txtBinearResult.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.txtBinearResult.Font = New System.Drawing.Font("Consolas", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBinearResult.ForeColor = System.Drawing.Color.White
+        Me.txtBinearResult.Location = New System.Drawing.Point(12, 464)
+        Me.txtBinearResult.Name = "txtBinearResult"
+        Me.txtBinearResult.Size = New System.Drawing.Size(708, 32)
+        Me.txtBinearResult.TabIndex = 6
+        Me.txtBinearResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'panelLoading
+        '
+        Me.panelLoading.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.panelLoading.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.panelLoading.Controls.Add(Me.lblPersent)
+        Me.panelLoading.Controls.Add(Me.Label5)
+        Me.panelLoading.Controls.Add(Me.ProgressBar1)
+        Me.panelLoading.Controls.Add(Me.Label4)
+        Me.panelLoading.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.panelLoading.Location = New System.Drawing.Point(310, 25)
+        Me.panelLoading.Name = "panelLoading"
+        Me.panelLoading.Size = New System.Drawing.Size(410, 433)
+        Me.panelLoading.TabIndex = 7
+        Me.panelLoading.Visible = False
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Consolas", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(84, 239)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(247, 19)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Data sedang diproses"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Consolas", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(84, 217)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(247, 25)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Mohon menunggu"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'cmdReset
+        '
+        Me.cmdReset.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdReset.Location = New System.Drawing.Point(186, 422)
+        Me.cmdReset.Name = "cmdReset"
+        Me.cmdReset.Size = New System.Drawing.Size(114, 36)
+        Me.cmdReset.TabIndex = 8
+        Me.cmdReset.Text = "Reset"
+        Me.cmdReset.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(60, 195)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(292, 21)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.ProgressBar1.TabIndex = 9
+        '
+        'lblPersent
+        '
+        Me.lblPersent.Font = New System.Drawing.Font("Consolas", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPersent.ForeColor = System.Drawing.Color.White
+        Me.lblPersent.Location = New System.Drawing.Point(84, 152)
+        Me.lblPersent.Name = "lblPersent"
+        Me.lblPersent.Size = New System.Drawing.Size(247, 40)
+        Me.lblPersent.TabIndex = 10
+        Me.lblPersent.Text = "Mohon menunggu"
+        Me.lblPersent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'frmCheckSum
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ClientSize = New System.Drawing.Size(746, 509)
+        Me.Controls.Add(Me.cmdReset)
+        Me.Controls.Add(Me.panelLoading)
+        Me.Controls.Add(Me.txtBinearResult)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtResult)
+        Me.Controls.Add(Me.cmdGenerate)
+        Me.Controls.Add(Me.txtBineary)
+        Me.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmCheckSum"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CheckSum"
+        Me.panelLoading.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtBineary As System.Windows.Forms.TextBox
+    Friend WithEvents cmdGenerate As System.Windows.Forms.Button
+    Friend WithEvents txtResult As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtBinearResult As System.Windows.Forms.TextBox
+    Friend WithEvents panelLoading As System.Windows.Forms.Panel
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents cmdReset As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents lblPersent As System.Windows.Forms.Label
+
+End Class
---
+++ checkSumApps/frmCheckSum.vb
@@ -0,0 +1,240 @@
+﻿Public Class frmCheckSum
+    Public Const jmlBit As Integer = 16
+    Dim hasilAkhir As String
+
+    Private Sub txtBineary_KeyDown(sender As Object, e As KeyEventArgs) Handles txtBineary.KeyDown
+        If e.KeyCode = 13 Then
+            cmdGenerate.PerformClick()
+        End If
+    End Sub 'menyimpan hasil per penjumlahan
+    Private Sub txtBineary_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtBineary.KeyPress
+        '48>0 49>1 8>backspace
+        If (Asc(e.KeyChar) <> 48 And Asc(e.KeyChar) <> 49 And Asc(e.KeyChar) <> 8) Then
+            e.KeyChar = Chr(0)
+        End If
+    End Sub
+
+    Private Sub frmCheckSum_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub cmdGenerate_Click(sender As Object, e As EventArgs) Handles cmdGenerate.Click
+        panelLoading.Visible = True
+        txtResult.Text = ""
+        txtBinearResult.Text = ""
+        Application.DoEvents()
+
+        Dim panjangInputan As Integer = Len(txtBineary.Text)
+        Dim sisaPembagian As Integer = (panjangInputan Mod jmlBit)
+        Dim perulangan As Integer = (Math.Floor(panjangInputan / jmlBit)) - 1
+
+        Dim hasil As String
+
+        If Val(sisaPembagian) <> 0 Then
+            perulangan = perulangan + 1
+        End If
+
+        '================= jika inputan kurang dari 16 karakter maka tambahkan angka 0 di depan agar memenuhi 16 karakter ==============
+        If panjangInputan < jmlBit Then
+            txtBineary.Text = Microsoft.VisualBasic.Right("0000000000000000" + txtBineary.Text, jmlBit)
+            panjangInputan = jmlBit
+        End If
+
+        txtResult.Text = ""
+        '====================== cetak result ke textbox result ===========================
+        txtResult.Text = txtResult.Text + vbCrLf + "       HASIL CHECKSUM DENGAN BILANGAN BINEAR " + jmlBit.ToString + " BIT"
+        txtResult.Text = txtResult.Text + vbCrLf + "========================================================="
+        txtResult.Text = txtResult.Text + vbCrLf + "Panjang inputan binear         : " + panjangInputan.ToString
+        txtResult.Text = txtResult.Text + vbCrLf + "Jumlah perulangan              : " + perulangan.ToString + " kali"
+        txtResult.Text = txtResult.Text + vbCrLf + "Sisa hasil bagi (MOD)          : " + sisaPembagian.ToString
+        txtResult.Text = txtResult.Text + vbCrLf + "========================================================="
+        txtResult.Text = txtResult.Text + vbCrLf + "               P    R    O    S    E    S"
+        txtResult.Text = txtResult.Text + vbCrLf + "=========================================================" + vbCrLf
+
+        '==================== jika hanya ada 16 karakter maka langsung di balik (proses checksum) =============
+        If perulangan = 0 Then
+            Dim bil = Microsoft.VisualBasic.Mid(txtBineary.Text, (panjangInputan - jmlBit + 1), jmlBit)
+            txtResult.Text = txtResult.Text + vbCrLf + "        BILANGAN         : " + bil.ToString
+            hasilAkhir = reverseBinear(txtBineary.Text).ToString
+            txtResult.Text = txtResult.Text + vbCrLf + "      "
+            txtResult.Text = txtResult.Text + vbCrLf + "        =====Convert -> Checksum====="
+            txtResult.Text = txtResult.Text + vbCrLf + "        CHECKSUM   : " + hasilAkhir.ToString
+            txtResult.Text = txtResult.Text + vbCrLf + "        ==============End============"
+            'tampilkan hasil di textbox
+            txtBinearResult.Text = "# CHECKSUM : " + hasilAkhir.ToString + " #"
+            panelLoading.Visible = False
+            Exit Sub
+        End If
+        '=======================================================================================================
+
+        For i As Integer = 1 To perulangan
+            Dim x As Integer
+            '====== menampilkan progress bar ====
+            x = i / perulangan * 100
+            Application.DoEvents()
+            ProgressBar1.Value = x
+            lblPersent.Text = x.ToString + "%"
+
+
+            txtResult.Text = txtResult.Text + vbCrLf + "PERULANGAN KE-" + i.ToString
+            Dim bil1, bil2 As String
+
+            '====================== penjumlahan baris pertama dan kedua =====================
+            If (i <> perulangan And i = 1 And perulangan > 1) Or (i = perulangan And i = 1 And perulangan = 1) Then
+                bil1 = Microsoft.VisualBasic.Mid(txtBineary.Text, (panjangInputan - jmlBit + 1), jmlBit)
+                txtResult.Text = txtResult.Text + vbCrLf + "        BILANGAN 1 : " + bil1.ToString
+
+                '===== cek jika jml bit yg diinput mencukupi atau kurang, kalau kurang ambil sisanya tambahkan 0 didepannya ======
+                If sisaPembagian = 0 Then
+                    If i = perulangan Then
+                        bil2 = Microsoft.VisualBasic.Mid(txtBineary.Text, 1, jmlBit)
+                    Else
+                        bil2 = Microsoft.VisualBasic.Mid(txtBineary.Text, (panjangInputan - (jmlBit * 2) + 1), jmlBit)
+                    End If
+                Else
+                    Dim xStart As Integer = (panjangInputan - (jmlBit * 2) + 1)
+                    Dim xjmlBit = jmlBit
+                    If xStart <= 0 Then
+                        xStart = 1
+                        xjmlBit = sisaPembagian
+                    End If
+
+                    bil2 = Microsoft.VisualBasic.Right("0000000000000000" + Microsoft.VisualBasic.Mid(txtBineary.Text, xStart, xjmlBit), jmlBit)
+                End If
+
+                txtResult.Text = txtResult.Text + vbCrLf + "        BILANGAN 2 : " + bil2.ToString
+
+                txtResult.Text = txtResult.Text + vbCrLf + "        ------------------------------------- +"
+                hasil = sumBinear(bil1, bil2)
+
+                txtResult.Text = txtResult.Text + vbCrLf + "        HASILNYA   : " + hasil
+                hasilAkhir = hasil
+
+                '=== cek hasilnya apakah melebihi 16 bit, jika iya maka wrap around ====
+                If Len(hasil) > jmlBit Then
+                    txtResult.Text = txtResult.Text + vbCrLf + wrapAround(hasil)
+                End If
+                '====================== end penjumlahan baris pertama dan kedua =====================
+
+            Else
+                '====================== penjumlahan berikutnya =====================
+                bil1 = hasilAkhir
+                txtResult.Text = txtResult.Text + vbCrLf + "        BILANGAN 1 : " + bil1.ToString
+
+                If i = perulangan Then
+                    If sisaPembagian = 0 Then
+                        bil2 = Microsoft.VisualBasic.Mid(txtBineary.Text, 1, jmlBit)
+                    Else
+                        bil2 = Microsoft.VisualBasic.Right("0000000000000000" + Microsoft.VisualBasic.Mid(txtBineary.Text, 1, sisaPembagian), jmlBit)
+                    End If
+                ElseIf i > 1 And perulangan > 1 Then
+                    bil2 = Microsoft.VisualBasic.Mid(txtBineary.Text, (panjangInputan - ((jmlBit * i) + 1)), jmlBit)
+                End If
+
+                hasil = sumBinear(bil1, bil2)
+
+                txtResult.Text = txtResult.Text + vbCrLf + "        BILANGAN 2 : " + bil2.ToString
+                txtResult.Text = txtResult.Text + vbCrLf + "        ------------------------------------- +"
+                txtResult.Text = txtResult.Text + vbCrLf + "        HASILNYA   : " + hasil
+
+                '=== cek hasilnya apakah melebihi 16 bit, jika iya maka wrap around ====
+                If Len(hasil) > jmlBit Then
+                    txtResult.Text = txtResult.Text + vbCrLf + wrapAround(hasil)
+                Else
+                    hasilAkhir = hasil
+                End If
+            End If
+
+            txtResult.Text = txtResult.Text + vbCrLf + "=========================================================" + vbCrLf
+        Next
+
+        txtResult.Text = txtResult.Text + vbCrLf + "        HASIL AKHIR: " + hasilAkhir.ToString
+        txtResult.Text = txtResult.Text + vbCrLf + "      "
+        txtResult.Text = txtResult.Text + vbCrLf + "        =====Convert -> Checksum====="
+        txtResult.Text = txtResult.Text + vbCrLf + "        CHECKSUM   : " + reverseBinear(hasilAkhir).ToString
+        txtResult.Text = txtResult.Text + vbCrLf + "        ==============End============"
+
+        'tampilkan hasil di textbox
+        txtBinearResult.Text = "# CHECKSUM : " + reverseBinear(hasilAkhir).ToString + " #"
+        panelLoading.Visible = False
+    End Sub
+
+    '=================================================================== baris fungsi ==================================================
+    Function sumBinear(ByVal binear1 As String, ByVal binear2 As String) As String
+        Dim result As String
+        Dim simpan As Integer = 0
+
+        For i As Integer = 1 To jmlBit
+            Dim bin1, bin2 As Integer
+            Dim hasil As Integer = 0
+
+            bin1 = Microsoft.VisualBasic.Mid(binear1, jmlBit - (i - 1), 1)
+            bin2 = Microsoft.VisualBasic.Mid(binear2, jmlBit - (i - 1), 1)
+
+            '==== jumlahkan dulu binear ke 1 dengan bilangan yg disimpan sebelumnya ====
+            hasil = simpan + bin1
+            If hasil = 2 Then
+                hasil = 0
+                simpan = 1
+            Else
+                simpan = 0
+            End If
+
+            '==== jumlahkan hasil dengan binear ke 2 ====
+            hasil = hasil + bin2
+            If hasil = 2 Then
+                hasil = 0
+                simpan = simpan + 1
+            Else
+                simpan = simpan + 0
+            End If
+
+            result = hasil.ToString + result
+
+            If simpan = 1 And i = jmlBit Then
+                result = simpan.ToString + result
+            End If
+        Next
+        Return result
+    End Function
+
+    Function wrapAround(ByVal binear As String) As String
+        Dim result As String
+
+        If Len(binear) > jmlBit Then
+            Dim bil1, bil2 As String
+            bil1 = Microsoft.VisualBasic.Mid(binear, (Len(binear) - jmlBit + 1), jmlBit)
+            bil2 = Microsoft.VisualBasic.Right("0000000000000000" + Microsoft.VisualBasic.Mid(binear, 1, (Len(binear) - jmlBit)), jmlBit)
+            result = "              -------------------------------------" + vbCrLf + "              - WRAP AROUND      : " + bil1
+            result = result + vbCrLf + "                                 : " + bil2
+            result = result + vbCrLf + "              ------------------------------------- +"
+            result = result + vbCrLf + "              - HASIL WRAP AROUND: " + sumBinear(bil1, bil2)
+            hasilAkhir = sumBinear(bil1, bil2)
+        End If
+
+        Return result
+    End Function
+    Function reverseBinear(ByVal binear As String) As String
+        Dim hasil As String = ""
+        For i As Integer = 1 To Len(binear)
+            Dim bil As Integer = Microsoft.VisualBasic.Mid(binear, (Len(binear) - (i - 1)), 1)
+            If bil = 0 Then
+                bil = 1
+            Else
+                bil = 0
+            End If
+            hasil = bil.ToString + hasil
+        Next
+        Return hasil
+    End Function
+
+    Private Sub cmdReset_Click(sender As Object, e As EventArgs) Handles cmdReset.Click
+        txtBinearResult.Text = ""
+        txtBineary.Text = ""
+        txtResult.Text = ""
+    End Sub
+
+    Private Sub txtBineary_TextChanged(sender As Object, e As EventArgs) Handles txtBineary.TextChanged
+
+    End Sub
+End Class
